adfab34c57e9050ee9ba1119a7729d65ac4d2bb5
Pieterjan De Clippel <pieterjandeclippel@hotmail.com>
2019-10-04 09:42:26 +0200

    Add test project

---
+++ KarnaughMap/KarnaughMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KarnaughMap
+{
+    public partial class KarnaughMap : UserControl
+    {
+        public KarnaughMap()
+        {
+            InitializeComponent();
+
+            Width = Height = 50;
+            Paint += KarnaughMap_Paint;
+        }
+
+        private void KarnaughMap_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(System.Drawing.Color.Black);
+        }
+    }
+}
---
+++ WindowsFormsApp1/KarnaughMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class KarnaughMap : UserControl
+    {
+        public KarnaughMap()
+        {
+            InitializeComponent();
+
+            Width = Height = 50;
+            Paint += KarnaughMap_Paint;
+        }
+
+        private void KarnaughMap_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(System.Drawing.Color.Black);
+        }
+    }
+}
